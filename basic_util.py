655437e68a7896be0980a0935b4c90420e4daef7
Kevin <kjplaye@gmail.com>
2016-12-03 13:13:00 -0500

    added some basic counting helpers

--- basic_util.py
+++ basic_util.py
@@ -4,6 +4,7 @@ from cols import transpose, col
 import pickle
 import random
 import numpy as np
+import scipy.stats
 
 def dim(x):
     if isinstance(x,str):
@@ -16,14 +17,6 @@ def dim(x):
 def blockup(x,n):
     return [x[i:i+n] for i in xrange(0,len(x)-n+1,n)]
 
-def entropy(x):
-    c = dict()
-    T = 0
-    for e in x:
-        c[e] = c.get(e,0) + 1
-        T+=1
-    P = [float(c[k])/T for k in c]
-    return -sum([math.log(p,2) * p for p in P])
 
 def rotate(x,n):
     return x[n:] + x[:n]
@@ -35,26 +28,6 @@ def reverse_it(x):
         L = x[:]
         L.reverse()
         return L
-
-def form_table(L,rowkeys = None,colkeys = None):
-    if rowkeys == None:
-      SL = set()
-      for l in L:
-        if l[0] not in SL:
-          SL.add(l[0])
-      rowkeys = sorted(list(SL))
-    VL = {rowkeys[i]:i for i in range(len(rowkeys))}
-    if colkeys == None:
-      SR = set()
-      for l in L:
-        if l[1] not in SR:
-          SR.add(l[1])
-      colkeys = sorted(list(SR))
-    VR = {colkeys[i]:i for i in range(len(colkeys))}
-    T = [[0 for j in range(len(colkeys))]  for i in range(len(rowkeys))]
-    for l in L:
-        T[VL[l[0]]][VR[l[1]]] += 1
-    return T
 
 def delta(x,d = 1):
     return [x[i+d] - x[i] for i in xrange(len(x) - d)]
@@ -80,9 +53,77 @@ def dither(x,e = 0.5):
   n = np.random.random(a.shape) * e
   return a + n
 
+
+
+
+
+def pval(x):
+    if hasattr(x,'pval'):
+        return x.pval()
+    if hasattr(x,'bits'):
+        return 2**-x.bits()
+
+def bits(x):
+    if hasattr(x,'bits'):
+        return x.bits()
+    if hasattr(x,'pval'):
+        return -math.log(x.pval(),2)
+
+    
+def entropy(x):
+    c = dict()
+    T = 0
+    for e in x:
+        c[e] = c.get(e,0) + 1
+        T+=1
+    P = [float(c[k])/T for k in c]
+    return -sum([math.log(p,2) * p for p in P])
+
+class form_table_result(col):
+    def pval(self):
+        return scipy.stats.chi2_contingency(self)[1]
+    def bits(self):
+        x, p, dof, ex = scipy.stats.chi2_contingency(self)
+        return -scipy.stats.chi2.logsf(x,dof) / math.log(2)
+        
+def form_table(L,rowkeys = None,colkeys = None):
+    if rowkeys == None:
+      SL = set()
+      for l in L:
+        if l[0] not in SL:
+          SL.add(l[0])
+      rowkeys = sorted(list(SL))
+    VL = {rowkeys[i]:i for i in range(len(rowkeys))}
+    if colkeys == None:
+      SR = set()
+      for l in L:
+        if l[1] not in SR:
+          SR.add(l[1])
+      colkeys = sorted(list(SR))
+    VR = {colkeys[i]:i for i in range(len(colkeys))}
+    T = [[0 for j in range(len(colkeys))]  for i in range(len(rowkeys))]
+    for l in L:
+        T[VL[l[0]]][VR[l[1]]] += 1
+    TAB = form_table_result(T)
+    return TAB
+
+class myhist_result(col):
+    def pval(self):
+        C = [e[1] for e in self]
+        x,p = scipy.stats.chisquare(C)
+        return p
+    def bits(self):
+        C = [e[1] for e in self]
+        x,p = scipy.stats.chisquare(C)
+        return -scipy.stats.chi2.logsf(x,len(C)-1)/math.log(2)
+
 def myhist(x,sortit = True):
   C = Counter(x)
   KV = [[k,C[k]] for k in C.keys()]
   if sortit:
     KV = sorted(KV,key = lambda x:x[1])
-  return col(KV)
+  return myhist_result(KV)
+
+
+
+
