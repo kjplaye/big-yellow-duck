b3cff21117e925c617403638313c246305a72ba6
Kevin Player <kjplaye@gmail.com>
2023-10-03 18:21:05 -0400

    PCA updates

--- pca.py
+++ pca.py
@@ -3,6 +3,7 @@ import scipy.stats
 from data_visualization import ggobi
 from math import log
 from matplotlib import pyplot as plt
+from basic_util import col
 
 # Regression model using (sqrt(m), sqrt(n), 1) for the max sv
 # of a random (m,n) matrix with iid entries from N(0,1)
@@ -19,18 +20,20 @@ def _max_sv_bits(m, n, max_sv):
     else:
     	sigma = 0.1
     zscore = (max_sv - mean) / sigma
-    return -scipy.stats.norm.logsf(zscore) / log(2.0), mean
+    return -scipy.stats.norm.logsf(zscore) / log(2.0), mean, zscore
 
 def _pca_bits(m,n,svs):
     ans = []
+    zscore_ans = []
     svs_array = np.array(svs)
     for k in range(len(svs)):
         rm_sigma = np.sum(svs_array[k:]**2 / (m*n))**0.5
-        (bits, mean0) = _max_sv_bits(m-k,n-k,svs[k] / rm_sigma)
+        (bits, mean0, zscr0) = _max_sv_bits(m-k,n-k,svs[k] / rm_sigma)
         if k==0:
             mean = mean0
         ans.append(bits)
-    return np.array(ans), mean
+        zscore_ans.append(zscr0)
+    return np.array(ans), mean, np.array(zscore_ans)
 
 class pca:
     """PCA
@@ -47,13 +50,19 @@ class pca:
     >>> print(P)
     PCA(128, 400)
     estimated rank = 1, mean =  31.08
-    ----------------------------------------
-    s_0 102.54 46158.92 bits
-        ............
-    s_1  30.53   0.55 bits
-    s_2  30.22   0.72 bits
-    s_3  29.96   1.12 bits
-    s_4  29.65   1.31 bits
+    
+    ind  MP  PV     sv   zscr     bits
+    ---  --  --     --   ----     ----
+    0 (M) (P) 102.53 251.72 45717.25
+    1          29.89  -3.51     0.00
+    2          29.68  -3.01     0.00
+    3          29.37  -2.90     0.00
+    4          29.18  -2.34     0.01
+    5          29.00  -1.72     0.06
+    6          28.77  -1.28     0.15
+    7          28.59  -0.68     0.41
+    8          28.22  -0.85     0.32
+    9          27.92  -0.75     0.37
     """
     def __init__(self, M, remove_mean = False):
     	self.M = M
@@ -61,9 +70,14 @@ class pca:
         	self.M -= np.mean(self.M,0)
     	[self.U, self.D, self.V] = np.linalg.svd(self.M,0)
     	[self.m, self.n] = self.M.shape
-    	self.pval, self.max_sv_mean = _pca_bits(self.m, self.n, self.D)
-    def estimated_rank(self, threshold_bits = 20.0):
-    	return np.argmin(self.pval >= threshold_bits)
+    	self.pval, self.max_sv_mean, self.zscore = _pca_bits(self.m, self.n, self.D)
+    def get_mp_threshold(self):
+    	return self.max_sv_mean * np.sum(self.D**2 / (self.m * self.n))**0.5
+    def estimated_rank(self, threshold_bits = 20.0, use_mp_thresh = True):
+        if use_mp_thresh:
+            return np.sum(self.D > self.get_mp_threshold())
+        else:
+            return np.argmin(self.pval >= threshold_bits)
     def plot_diag(self):
     	plt.plot(self.D,'o', color = 'blue')
     	plt.plot(self.D,'.', color = 'red')
@@ -75,12 +89,17 @@ class pca:
         	return ggobi(self.U[:,:dims])
     	else:
         	return ggobi(self.U[:,:dims], input_clusters)
-    def __repr__(self):
-        header_bar = '-' * 40
-        middle_bar = '    ............'
-        L = ['s_%d %6.2f %6.2f bits' % (k, self.D[k], self.pval[k]) for k in range(min(10,len(self.D)))]
+    def __repr__(self, threshold_bits = 20.0):
+        mp_thresh = self.get_mp_threshold()
+        header = ['ind','MP','PV','sv','zscr','bits']
+        header_lines = ['---','--','--','--','----','----']
+        L = [[k,
+              '(M)' if self.D[k] > mp_thresh else '   ',
+              '(P)' if self.pval[k] > threshold_bits else '   ',
+              "%6.2f" % self.D[k],
+              "%6.2f" % self.zscore[k],
+              "%6.2f" % self.pval[k]] for k in range(min(10,len(self.D)))]
         est_rank = self.estimated_rank()
-        L = [header_bar] + L[:est_rank] + [middle_bar] + L[est_rank:]
-        return f'PCA({self.m}, {self.n})\nestimated rank = {self.estimated_rank()}, mean = {"%6.2f" % self.max_sv_mean}\n' + '\n'.join(L)
-   	 
-   	 
+        LH = [header] + [header_lines] + L
+        begin = f'PCA({self.m}, {self.n})\nestimated rank = {self.estimated_rank()}, mean = {"%6.2f" % self.max_sv_mean}\n\n'
+        return begin + str(col(LH))
