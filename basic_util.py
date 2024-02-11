22597ed8cc3f4ca831b199307851ffbbfc9d4241
Kevin Player <kjplaye@gmail.com>
2024-02-11 15:03:11 -0500

    Cat update

--- basic_util.py
+++ basic_util.py
@@ -161,17 +161,48 @@ def make_patches(colors, labels):
     plt.legend(handles=patch)
 
 class Cat(np.ndarray):
-    def __new__(self, data):
-        cat = pd.Categorical(data)                       
+    """Expose some pandas functionality to consistently index categorical data.
+    
+    Usage Example:
+    >>> cat = Cat(['a','c','b','a'])
+    >>> print(cat)
+    [0, 2, 1, 0]
+
+    >>> print("Number of categories:", cat.num_cat)
+    Number of categories: 3
+
+    >>> print("Categories:", cat.cat)
+    Categories: Index(['a', 'b', 'c'], dtype='object')
+
+    >>> new_cat = cat.index(['c','a'])
+    >>> print("Pull in new data:", new_cat))
+    >>> print("As a series:")
+    >>> print(new_cat.series)
+    >>> print("Lift to names:", new_cat.lift())
+    Pull in new data: [2 0]
+    As a series:
+    0   c
+    1   a
+    dtype: category
+    Categories (3, object): ['a', 'b', 'c']
+    Lift to names: Index(['c', 'a'], dtype='object')
+    """
+    def __new__(self, data, categories = None):
+        cat = pd.Categorical(data, categories = categories)
         series = pd.Series(cat)
         codes = series.cat.codes
         obj = np.asarray(codes.values).view(self)
-        obj.cat = cat
         obj.series = series
-        obj.codes = codes
-        obj.C = cat.categories
-        obj.n = cat.categories.nunique()
+        obj.cat = cat.categories
+        obj.num_cat = obj.cat.nunique()
         return obj
+    def index(self, data):
+        """Apply Cat indexing to new data."""
+        return Cat(data, categories=self.cat)
+    def lift(self):
+        return self.cat[self]
+    def __hash__(self):
+        return hash((tuple(self), tuple(self.cat)))
 
 def compose_advice(list_of_arrays, array_names = None):
     """
